fe7431d8ab3d4387bae67432e90c2312fd6d44c5
suyashekhande <suyashekhande@gmail.com>
2024-11-01 13:11:13 +0530

    Implemented Morgan Request Logger

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const cors = require("cors");
+const morgan = require('morgan');
 const app = express()
 const port = 3000
-app.use(cors());
 app.use(express.json());
+
+// Custom token to capture request body
+morgan.token('body', (req) => JSON.stringify(req.body));
+
+// Define a custom Morgan format
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
+app.use(cors());
 
 const mainRouter = require('./routes/index')
 app.use("/api/v1", mainRouter);
--- middleware.js
+++ middleware.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    console.log(authHeader)
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ message: "Unauthorized Token" });
